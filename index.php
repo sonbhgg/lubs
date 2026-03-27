b13163ba3427fe1d35964cd5256826d10336f271
Veronica <akim60452@gmail.com>
2026-03-27 10:33:14 +0300

    Добавлен код третьего задания первой части

--- index.php
+++ index.php
@@ -14,3 +14,41 @@ try {
     echo "Исключение: " . $ex->getMessage() . "<br>";
     echo "В файле: " . $ex->getFile() . " в строке: " . $ex->getLine();
 }
+
+
+
+$dividend = 10;
+$divisor = 0;
+
+try {
+    if ($divisor == 0) {
+        throw new Exception("Попытка деления на ноль.");
+    }
+    
+    $result = $dividend / $divisor;
+    echo "Результат деления: " . $result;
+
+} catch (Exception $ex) {
+    $errorMessage = date("Y-m-d H:i:s") . " - Ошибка: " . $ex->getMessage() . " в файле " . $ex->getFile() . " на строке " . $ex->getLine() . PHP_EOL;
+    
+    file_put_contents("log.txt", $errorMessage, FILE_APPEND);
+    
+    echo "<br>" . "Произошла ошибка. Подробности записаны в лог-файл.";
+}
+
+
+
+$countries = ['Spain' => 'Madrid', 'Russia' => 'Moscow'];
+$requestedCountry = 'Germany';
+
+try {
+    if (!array_key_exists($requestedCountry, $countries)) {
+        throw new Exception("Страна '{$requestedCountry}' не найдена в массиве.");
+    }
+    
+    $capital = $countries[$requestedCountry];
+    echo "Столица страны {$requestedCountry}: {$capital}";
+
+} catch (Exception $ex) {
+    echo "<br>" . "Исключение: " . $ex->getMessage();
+}
