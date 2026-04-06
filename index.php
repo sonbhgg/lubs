e5bf64b5356daf963eb3ff8cdebc30bdfc25133d
Veronica <akim60452@gmail.com>
2026-04-06 11:13:23 +0300

    Задание 1

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+class Worker
+{
+    // Задание 1: класс и свойства
+    private $name;
+    private $age;
+    private $salary;
+    
+}
+
+// Задание 1: 2 объекта класса
+$worker1 = new Worker("Иван Петров", 25, 50000);
+$worker2 = new Worker("Мария Сидорова", 19, 60000);
+
+?>
