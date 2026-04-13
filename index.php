a954e2ad7cb435babbd25d494a1e06f800d1821e
Veronica <akim60452@gmail.com>
2026-04-13 15:19:31 +0300

    Задание 12-13

--- index.php
+++ index.php
@@ -92,3 +92,24 @@ class Triangle extends Figure implements AreaCalculable
         return "Это класс треугольника. У него {$this->sidesCount} стороны.";
     }
 }
+
+echo "<h3>Прямоугольники</h3>";
+$rect1 = new Rectangle(5, 10, 'красный');
+$rect2 = new Rectangle(3, 7, 'синий');
+
+echo $rect1->infoAbout() . " Площадь: " . $rect1->getArea() . "<br>";
+echo $rect2->infoAbout() . " Площадь: " . $rect2->getArea() . "<br>";
+
+echo "<h3>Квадраты</h3>";
+$square1 = new Square(4, 'зелёный');
+$square2 = new Square(6, 'жёлтый');
+
+echo $square1->infoAbout() . " Площадь: " . $square1->getArea() . "<br>";
+echo $square2->infoAbout() . " Площадь: " . $square2->getArea() . "<br>";
+
+echo "<h3>Треугольники</h3>";
+$tri1 = new Triangle(3, 4, 5, 'оранжевый');
+$tri2 = new Triangle(5, 5, 6, 'фиолетовый');
+
+echo $tri1->infoAbout() . " Площадь: " . $tri1->getArea() . "<br>";
+echo $tri2->infoAbout() . " Площадь: " . $tri2->getArea() . "<br>";
