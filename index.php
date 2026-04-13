0e2b8af99c3b87efb08117fbe040d53eb8771e06
Veronica <akim60452@gmail.com>
2026-04-13 15:14:40 +0300

    Задание 8-9

--- index.php
+++ index.php
@@ -9,6 +9,11 @@ abstract class Figure
     protected $color;      
     protected $sidesCount; 
     
+    public function __construct($color = 'белый')
+    {
+        $this->color = $color;
+    }
+    
     abstract public function infoAbout();
 }
 
@@ -16,11 +21,26 @@ class Rectangle extends Figure implements AreaCalculable
 {
     private $a;
     private $b;
+    
+    public function __construct($a, $b, $color = 'белый')
+    {
+        parent::__construct($color);
+        $this->a = $a;
+        $this->b = $b;
+        $this->sidesCount = 4;
+    }
 }
 
 class Square extends Figure implements AreaCalculable
 {
     private $a;
+    
+    public function __construct($a, $color = 'белый')
+    {
+        parent::__construct($color);
+        $this->a = $a;
+        $this->sidesCount = 4;
+    }
 }
 
 class Triangle extends Figure implements AreaCalculable
@@ -28,4 +48,13 @@ class Triangle extends Figure implements AreaCalculable
     private $a;
     private $b;
     private $c;
+    
+    public function __construct($a, $b, $c, $color = 'белый')
+    {
+        parent::__construct($color);
+        $this->a = $a;
+        $this->b = $b;
+        $this->c = $c;
+        $this->sidesCount = 3;
+    }
 }
